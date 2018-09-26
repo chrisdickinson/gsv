7a8a037abd71a9b033b31ae8d4cd4c18de185f88
Chris Dickinson <christopher.s.dickinson@gmail.com>
2018-09-26 11:12:48 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# gsv: github search vehicle
+
+```
+$ npx gsv 'some code'
+```
+
+## License
+
+MIT
---
+++ gsv.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const toml = require('@iarna/toml')
+const rc = require('rc')
+
+const config = rc('gsv', {
+  url: 'https://api.github.com'
+}, [], xs => toml.parse(xs))
+
+if (!config.token) {
+  const setup = require('./lib/setup.js')
+
+  return setup(config.url).catch(err => {
+    process.stderr.write(err.message)
+    process.exit(1)
+  })
+}
+
+const GitHubClient = require('./lib/github-client')
+
+require('./lib/search')({
+  argv: process.argv.slice(2),
+  client: new GitHubClient({
+    url: config.url,
+    defaults: config.defaults,
+    username: config.username,
+    token: config.token
+  })
+}).catch(err => {
+  process.stderr.write(err.message)
+  process.exit(1)
+})
